3e70c7b2becb1814b3c3625185f8eab4ceeefab5
Cola <cscolabear@gmail.com>
2018-02-20 17:29:48 +0800

    [update] 更新 安裝用 shell script - 建立 symlink

--- brew_install.sh
+++ brew_install.sh
@@ -8,35 +8,38 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 
 echo
-echo "--- install Start... ---"
+echo "${green}------------------------${reset}"
+echo "${green}--- install Start... ---${reset}"
+echo "${green}------------------------${reset}"
 echo
 
 # If Homebrew is not installed
 if ! which brew > /dev/null; then
-	echo "* Install Homebrew..."
+	echo "${green}* Install Homebrew...{$reset}"
      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi;
 
 echo
-echo "* brew update...."
-# Update brew
-brew update
+echo "${green}* brew update....${reset}"
+echo
+# brew update
 
 # Install everything in Brewfile
-brew bundle
-
-# Remove outdated versions from the cellar
-brew cleanup
+# brew bundle
+# brew cleanup
 
 
 
+#
 # cp bash_profile
+#
 echo
-echo "* check bash_profile..."
+echo "${green}* check bash_profile...${reset}"
+echo
 file="/Users/$USER/.bash_profile"
 source_file="bash_profile"
 
-echo "is exist : $file"
+echo " - is exist : $file"
 if [ -e "$file" ]; then
     echo "${green}files found.${reset}"
 else
@@ -53,20 +56,42 @@ echo
 
 
 
+#
+# create symlink
+#
 echo
-echo "* create symlink...."
+echo "${green}* create symlink....${reset}"
+echo
+
 # symlink to subl
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
 
+# Navicat connect list
+ln -s ~/Documents/Navicat/preference.plist ~/Library/Application\ Support/PremiumSoft\ CyberTech
+
+# Sequel Pro
+ln -s ~/Documents/Sequel\ Pro/Favorites.plist ~/Library/Application\ Support/Sequel\ Pro/Data
+ln -s ~/Documents/Sequel\ Pro/com.sequelpro.SequelPro.plist ~/Library/Preferences
+
+# Transmit
+ln -s ~/Documents/Transmit/Favorites ~/Library/Application\ Support/Transmit
+ln -s ~/Documents/Transmit/Metadata ~/Library/Application\ Support/Transmit
 
 
+
+#
 # Allow Apps from Anywhere in macOS
-echo ""
-echo "* spctl --master-disable : "
+#
+echo
+echo "${green}* spctl --master-disable : ${reset}"
+echo
 sudo spctl --master-disable
 spctl --status
 
 
+
 echo
-echo "--- Done. ---"
+echo "${green}-------------${reset}"
+echo "${green}--- Done. ---${reset}"
+echo "${green}-------------${reset}"
 echo
