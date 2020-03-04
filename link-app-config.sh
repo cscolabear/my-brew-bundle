69abb1fd05a113460c3e61fe30e908f88fb87ed7
Cola <cscolabear@gmail.com>
2020-03-04 17:51:53 +0800

    change link sh

---
+++ link-app-config.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#
+# Install all the things with Homebrew, Casks and a Brewfile
+#
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+echo
+echo "${green}---------------------------${reset}"
+echo "${green}--- create link for misc ---${reset}"
+echo "${green}---------------------------${reset}"
+echo
+
+
+project="Sequel Pro"
+source_file="/Users/$USER/Dropbox/Sync/Sequel Pro/Data"
+target_file="/Users/$USER/Library/Application Support/Sequel Pro/Data"
+
+echo " - 🔍${red}${project}${reset} target: is exist? : \`${green}${target_file}${reset}\`"
+if [ -e "${target_file}" ]; then
+    echo "⭕️${green}target exist${reset}"
+
+else
+    echo " - source file: is exist? : \`${green}${source_file}${reset}\`"
+    if [ -e "${source_file}" ]; then
+        ln -s "${source_file}" "${target_file}"
+        echo "⭕️${green}link created!${reset}"
+
+    else
+        echo "❌${red}source not exist...${reset}"
+
+    fi
+
+fi
+# ^
+
+project="ml .env"
+source_file="/Users/$USER/Dropbox/Sync/SourceTree/browser.plist"
+target_file="/Users/$USER/Library/ApplicationSupport/SourceTree/browser.plist"
+# for SourceTree, used `ln` not `ln -s`
+ln "${source_file}" "${target_file}"
+
+
+echo
+echo "${green}-------------${reset}"
+echo "${green}--- Done. ---${reset}"
+echo "${green}-------------${reset}"
+echo
--- link-ml-project.sh
+++ link-ml-project.sh
@@ -13,34 +13,6 @@ echo "${green}--- create link for misc ---${reset}"
 echo "${green}---------------------------${reset}"
 echo
 
-
-project="Sequel Pro"
-source_file="/Users/$USER/Dropbox/Sync/Sequel Pro/Data"
-target_file="/Users/$USER/Library/Application Support/Sequel Pro/Data"
-
-function createLink {
-    ln -s "${1}" "${2}"
-}
-
-echo " - 🔍${red}${project}${reset} target: is exist? : \`${green}${target_file}${reset}\`"
-if [ -e "${target_file}" ]; then
-    echo "⭕️${green}target exist${reset}"
-
-else
-    echo " - source file: is exist? : \`${green}${source_file}${reset}\`"
-    if [ -e "${source_file}" ]; then
-        createLink "${source_file}" "${target_file}"
-        echo "⭕️${green}link created!${reset}"
-
-    else
-        echo "❌${red}source not exist...${reset}"
-
-    fi
-
-fi
-# ^
-
-
 project="ml .env"
 source_file="/Users/$USER/Dropbox/Sync/upn/Projects/ml.env"
 target_file="/Users/$USER/Projects/mamilove.com.tw/.env"
